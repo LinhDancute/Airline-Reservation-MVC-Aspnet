11ca8fc27a01b76058f64c961185e6b6cbb1a990
LinhDancute <dannguyen11003@gmail.com>
2023-11-09 17:01:43 +0700

    Create Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Airline_Reservation_Vietjet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
